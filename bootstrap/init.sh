3040c2ade920daf2041dc566472b9d45183822ea
veermuchandi <veermuchandi@google.com>
2023-01-11 21:44:12 +0000

    removed bootstrap

--- bootstrap/init.sh
+++ bootstrap/init.sh
@@ -8,7 +8,7 @@ fi
 # sets the current project for gcloud
 gcloud config set project $PROJECT_ID
 
-source bootstrap/env.sh
+source env.sh
 
 # Enables various APIs you'll need
 gcloud services enable container.googleapis.com cloudbuild.googleapis.com \
